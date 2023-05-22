55b7895e09ed10207cee46f4940aa63693c99b57
sem.langhi@icloud.com <samuele.langhi@mail.polimi.it>
2023-05-22 16:41:22 +0200

    tuing

--- src/main/java/NCOSQAGraphStockCombo.java
+++ src/main/java/NCOSQAGraphStockCombo.java
@@ -84,7 +84,7 @@ public class NCOSQAGraphStockCombo {
                     public long extract(ConsumerRecord<Object, Object> record, long partitionTime) {
                         return ((Stock)record.value()).getTs();
                     }
-                }, Topology.AutoOffsetReset.EARLIEST)), timeWindows.size(), timeWindows.advanceMs, new SpeedConstraintStockValueFactory(0.00000001/constraintStrictness, -0.00000001/constraintStrictness));
+                }, Topology.AutoOffsetReset.EARLIEST)), timeWindows.size(), timeWindows.advanceMs, new SpeedConstraintStockValueFactory(0.1/constraintStrictness, -0.1/constraintStrictness));
 
 
         ApplicationSupplier applicationSupplier = new ApplicationSupplier(1);
